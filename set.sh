053473286ce7193519342c792a9880ceeaa21417
Werner, Lorenz <Lorenz.Werner@sprengnetter.de>
2018-05-04 10:14:15 +0200

    suppressed warnings in run.sh script

--- set.sh
+++ set.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
-#xkbcomp -I$HOME/config/xkb ~/config/xkb/map $DISPLAY
-xkbcomp -I$(dirname $0) $(dirname $0)/map $DISPLAY
+xkbcomp -w0 -I$(dirname $0) $(dirname $0)/map $DISPLAY
